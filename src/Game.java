48bea688ae65ebf7dd3fcd5d9acbf7bdd769f785
Polina <yatsko_polina1@mail.ru>
2022-08-07 18:32:24 +0300

    Gamers activity

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,32 @@
 import java.util.Scanner;
 public class Game {
-        Scanner in=new Scanner(System.in);
-        public static void main(String[] args) {
-            for (int a=0;a<5;a++){
-                if (a%2==0){
-                    System.out.println(" | | ");
-                }
-                else {
-                    System.out.println("-------");
-                }
+
+    public static void main(String[] args) {
+        scheme();
+        Gamers tom = new Gamers();
+        for (int i = 0; i < 4; i++) {
+            tom.gamer1();
+            tom.gamer2();
+        }
+    }
+    public static void scheme() {
+        for (int a = 0; a < 5; a++) {
+            if (a % 2 == 0) {
+                System.out.println(" | | ");
+            } else {
+                System.out.println("-------");
             }
         }
     }
+}
+class Gamers{
+    Scanner in = new Scanner(System.in);
+        public void gamer1 () {
+        System.out.print("Игрок 1:");
+        String gamer1 = in.nextLine();
+    }
+        public void gamer2 () {
+        System.out.print("Игрок 2:");
+        String gamer2 = in.nextLine();
+    }
+}
