9487611fe22712769f419553183119ab7bd5eb19
Polina <yatsko_polina1@mail.ru>
2022-08-11 20:44:51 +0300

    Gamers activity

--- src/Game.java
+++ src/Game.java
@@ -1,32 +1,37 @@
 import java.util.Scanner;
 public class Game {
-
     public static void main(String[] args) {
-        scheme();
-        Gamers tom = new Gamers();
+        int [][] position=new int [][]{{1,2,3},{4,5,6},{7,8,9}};
+        for (int i=0;i<position.length;i++){
+            System.out.println();
+            for(int j=0;j<position.length;j++){
+                System.out.print("| "+position[i][j]+" |");
+            }
+            System.out.println("\n--------------");
+        }
+        Gamers move = new Gamers();
         for (int i = 0; i < 4; i++) {
-            tom.gamer1();
-            tom.gamer2();
+            move.gamer1();
+            move.gamer2();
+        }
         }
     }
-    public static void scheme() {
-        for (int a = 0; a < 5; a++) {
-            if (a % 2 == 0) {
-                System.out.println(" | | ");
-            } else {
-                System.out.println("-------");
+class Gamers {
+    Scanner in = new Scanner(System.in);
+    public String gamer1() {
+        System.out.print("Введите номер свободного поля от 1 до 9");
+        int a = in.nextInt();
+        for (int i = 0; i < position.length; i++) {
+            for (int j = 0; j < position.length; j++) {
+                if (a == int position[ i][j]){
+                    return "o";
+                }
             }
         }
     }
-}
-class Gamers{
-    Scanner in = new Scanner(System.in);
-        public void gamer1 () {
-        System.out.print("Игрок 1:");
-        String gamer1 = in.nextLine();
-    }
-        public void gamer2 () {
+        public String gamer2 () {
         System.out.print("Игрок 2:");
         String gamer2 = in.nextLine();
+        return "x";
     }
 }
